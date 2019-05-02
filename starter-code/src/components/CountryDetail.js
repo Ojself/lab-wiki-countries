901dc240b830671e0924081a4c00f67d26efbe43
Tormod Flesjø <tormod.flesjo@gmail.com>
2019-05-02 12:18:52 +0200

    instructor code

---
+++ starter-code/src/components/CountryDetail.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import NothingSelect from "./NothingSelect.js";
+import Info from "./Info.js";
+class CountryDetail extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={NothingSelect} />
+          <Route path="/:id" component={Info} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default CountryDetail;
